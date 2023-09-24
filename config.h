3fa15ee8c12a76943e24ca9e7338b1c7776593af
Gergely Bencsik <becsegergely@gmail.com>
2023-09-24 15:53:50 +0200

    Neon vomit added

---
+++ config.h
@@ -0,0 +1,23 @@
+#ifndef __EXO_COLOR_CONFIG_H__
+#define __EXO_COLOR_CONFIG_H__
+
+#define BLACK_DARK      "#000000"
+#define RED_DARK        "#ff0066"
+#define GREEN_DARK      "#00ff66"
+#define YELLOW_DARK     "#ffaa00"
+#define BLUE_DARK       "#0066ff"
+#define MAGENTA_DARK    "#5a00ff"
+#define CYAN_DARK       "#00aedd"
+#define WHITE_DARK      "#acc9ce"
+
+#define BLACK_LIGHT     "#0066ff"
+#define RED_LIGHT       "#ff0033"
+#define GREEN_LIGHT     "#00ff99"
+#define YELLOW_LIGHT    "#ffcc00"
+#define BLUE_LIGHT      "#0099ff"
+#define MAGENTA_LIGHT   "#cc00ff"
+#define CYAN_LIGHT      "#00ffcc"
+#define WHITE_LIGHT     "#acc9ce"
+
+
+#endif //__EXO_COLOR_CONFIG_H__
