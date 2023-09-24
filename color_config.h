ea6c5a9867794106c3acf56d6f79d17e252043b9
Gergely Bencsik <becsegergely@gmail.com>
2023-09-24 18:54:46 +0200

    Macros changed to static const vars

--- color_config.h
+++ color_config.h
@@ -1,23 +1,23 @@
 #ifndef __EXO_COLOR_CONFIG_H__
 #define __EXO_COLOR_CONFIG_H__
 
-#define BLACK_DARK      "#000000"
-#define RED_DARK        "#ff0066"
-#define GREEN_DARK      "#00ff66"
-#define YELLOW_DARK     "#ffaa00"
-#define BLUE_DARK       "#0066ff"
-#define MAGENTA_DARK    "#5a00ff"
-#define CYAN_DARK       "#00aedd"
-#define WHITE_DARK      "#acc9ce"
+static const char BLACK_DARK[]      = "#000000";
+static const char RED_DARK[]        = "#ff0066";
+static const char GREEN_DARK[]      = "#00ff66";
+static const char YELLOW_DARK[]     = "#ffaa00";
+static const char BLUE_DARK[]       = "#0066ff";
+static const char MAGENTA_DARK[]    = "#5a00ff";
+static const char CYAN_DARK[]       = "#00aedd";
+static const char WHITE_DARK[]      = "#acc9ce";
 
-#define BLACK_LIGHT     "#0066ff"
-#define RED_LIGHT       "#ff0033"
-#define GREEN_LIGHT     "#00ff99"
-#define YELLOW_LIGHT    "#ffcc00"
-#define BLUE_LIGHT      "#0099ff"
-#define MAGENTA_LIGHT   "#cc00ff"
-#define CYAN_LIGHT      "#00ffcc"
-#define WHITE_LIGHT     "#acc9ce"
+static const char BLACK_LIGHT[]     = "#0066ff";
+static const char RED_LIGHT[]       = "#ff0033";
+static const char GREEN_LIGHT[]     = "#00ff99";
+static const char YELLOW_LIGHT[]    = "#ffcc00";
+static const char BLUE_LIGHT[]      = "#0099ff";
+static const char MAGENTA_LIGHT[]   = "#cc00ff";
+static const char CYAN_LIGHT[]      = "#00ffcc";
+static const char WHITE_LIGHT[]     = "#acc9ce";
 
 
 #endif //__EXO_COLOR_CONFIG_H__
